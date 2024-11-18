4129f7728fecc2d08776ade341fdab5c65da682f
Odin Thif <mjd.thif@icloud.com>
2024-11-18 22:05:30 +0100

    reuseble components | heading

---
+++ src/app/reuseble-components/heading.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { IconType } from "react-icons";
+
+type Props = {
+  title: string;
+  subtitle?: string;
+  center?: boolean;
+  icon?: IconType;
+  iconSize?: number;
+};
+
+export const Heading = ({
+  title,
+  subtitle,
+  center,
+  icon: Icon,
+  iconSize,
+}: Props) => {
+  // Rendering the Heading component with optional subtitle, center alignment, and icon
+  return (
+    <div className={center ? "text-center" : "text-start"}>
+      {Icon && <Icon size={iconSize || 24} />}{" "}
+      {/* Optional icon with customizable size */}
+      <div className="text-3xl font-bold">{title}</div>
+      {subtitle && (
+        <div className="font-light text-neutral-500 mt-2">{subtitle}</div>
+      )}
+    </div>
+  );
+};
