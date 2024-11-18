e402b2a9c79f8fb00baef4a297e9b0701bfe522e
Odin Thif <mjd.thif@icloud.com>
2024-11-18 22:07:06 +0100

    reuseble component | input

--- src/app/reuseble-components/heading.tsx
+++ src/app/reuseble-components/heading.tsx
@@ -16,11 +16,9 @@ export const Heading = ({
   icon: Icon,
   iconSize,
 }: Props) => {
-  // Rendering the Heading component with optional subtitle, center alignment, and icon
   return (
     <div className={center ? "text-center" : "text-start"}>
       {Icon && <Icon size={iconSize || 24} />}{" "}
-      {/* Optional icon with customizable size */}
       <div className="text-3xl font-bold">{title}</div>
       {subtitle && (
         <div className="font-light text-neutral-500 mt-2">{subtitle}</div>
---
+++ src/app/reuseble-components/input.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+
+type Props = {
+  id: string;
+  label?: string;
+  placeholder?: string;
+  type?: string;
+  disabled?: boolean;
+  register: any;
+  onFocus?: () => void;
+  onBlur?: () => void;
+};
+
+export const Input = ({
+  id,
+  label,
+  placeholder,
+  type,
+  disabled,
+  register,
+  onFocus,
+  onBlur,
+}: Props) => {
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown);
+  };
+  return (
+    <div className="w-full relative my-2">
+      <input
+        id={id}
+        disabled={disabled}
+        {...register(id)}
+        placeholder=""
+        type={
+          type === "password" ? (passwordShown ? "text" : "password") : type
+        }
+        className={`peer relative z-10 w-full p-4 pl-4 font-light bg-transparent border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed `}
+      />
+      {type &&
+        type === "password" &&
+        (passwordShown ? (
+          <AiFillEyeInvisible
+            className="absolute top-5 right-5 cursor-pointer z-40"
+            size={30}
+            onClick={togglePassword}
+          />
+        ) : (
+          <AiFillEye
+            className="absolute top-5 right-5 cursor-pointer z-40"
+            size={30}
+            onClick={togglePassword}
+          />
+        ))}
+      <label
+        className={`absolute text-sm duration-150 transform -translate-y-4 top-5 left-4 z-9 origin-[0] peer-placeholder-shown:scale-75 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 `}
+        htmlFor={id}
+      >
+        {label}
+      </label>
+    </div>
+  );
+};
