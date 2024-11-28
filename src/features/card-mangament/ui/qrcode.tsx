64d487fbaf8cd04e872e766d59cd96f57e8be94c
Odin Thif <mjd.thif@icloud.com>
2024-11-28 19:53:15 +0100

    added next public key

--- src/features/card-mangament/ui/qrcode.tsx
+++ src/features/card-mangament/ui/qrcode.tsx
@@ -1,6 +1,6 @@
 import QRCode from "react-qr-code";
 export function QRCodeCard({ userId }: { userId: string }) {
-  const url = process.env.NEXT_PUBLIC_BASE_URL!;
+  // const url = process.env.NEXT_PUBLIC_BASE_URL!;
   return (
     <div className="bg-white p-2 flex justify-center items-center rounded-lg">
       <QRCode
